aed4ef58f586066c54759c37e6dba39e62bc95e2
Maynard <elimaynard923@gmail.com>
2022-04-22 02:15:36 -0500

    add mermaidJS support

--- fmt/flossy-2/format.js
+++ fmt/flossy-2/format.js
@@ -970,6 +970,22 @@ commands.quote = function(s) {
   return new Rep.Seq('<blockquote>', body, '</blockquote>');
 }
 
+// MermaidJS support
+commands.mermaid = function(s) {
+  p_spaces(s);
+  const [body, _] = p_enclosed(s, p_toplevel_verbatim);
+
+  // Unfortunately, mermaid does not seem to offer an API for rendering
+  // a chart outwide of a browser environment.
+  // So instead we have to defer rendering to the client.
+
+  const divId = s.gensym('mermaid');
+  return new Rep.Seq(String.raw`
+    <div id="${divId}">${escapeHtml(body)}</div>
+    <script> window.renderMermaid('${divId}'); </script>
+  `);
+}
+
 
 // Jargon-lead implicit references
 function p_implicitReference(s) {
@@ -1363,6 +1379,8 @@ jargonImplementation,
 
 expandableListsImplementation,
 
+mermaidJsImplementation,
+
 `
 
 <main>`, html, `</main>
@@ -1649,6 +1667,42 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
 });
+
+</script>
+
+`;
+
+const mermaidJsImplementation = String.raw`
+
+<script>
+
+window.renderMermaid = async function(eltId) {
+  await loadMermaid();
+  mermaid.mermaidAPI.initialize({ startOnLoad: false });
+    // ^ Yes, we have to reload and reinitialize every time.
+    //   No, I don't know why.
+
+  await domContentLoaded;
+
+  const $in = document.getElementById(eltId);
+  const $out = document.createElement('div');
+  $out.style.textAlign = 'center';
+  $in.after($out);
+
+  mermaid.mermaidAPI.render($in.id, $in.innerText, r => { $out.innerHTML = r; });
+}
+
+const domContentLoaded = new Promise(resolve =>
+  document.addEventListener('DOMContentLoaded', () => resolve()));
+
+async function loadMermaid() {
+  const $script = document.createElement('script');
+  $script.src = 'https://cdn.jsdelivr.net/npm/mermaid/dist/mermaid.min.js';
+  await new Promise(resolve => {
+    $script.onload = () => resolve();
+    document.body.append($script);
+  });
+}
 
 </script>
 
