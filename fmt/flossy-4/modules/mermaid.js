91f007eab7c79cbd2371f5ef7734b682f623c2c5
Maynard <me@maynards.site>
2022-11-21 16:36:12 -0800

    flossy-4 (remove Cats)

---
+++ fmt/flossy-4/modules/mermaid.js
@@ -0,0 +1,58 @@
+const { squire } = require('../../../squire.js');
+const { escapeHtml } = require('../util.js');
+const repm = require('../repm.js');
+const p = require('../parse.js');
+
+exports.commands = {};
+
+// MermaidJS support
+exports.commands.mermaid = function(s) {
+  p.p_spaces(s);
+  const [body, _] = p.p_enclosed(s, p.p_toplevel_verbatim);
+
+  // Unfortunately, mermaid does not seem to offer an API for rendering
+  // a chart outwide of a browser environment.
+  // So instead we have to defer rendering to the client.
+
+  const divId = p.gensym(s, 'mermaid');
+  return new repm.Seq(String.raw`
+    <div id="${divId}">${escapeHtml(body)}</div>
+    <script> window.renderMermaid('${divId}'); </script>
+  `);
+}
+
+exports.prelude = String.raw`
+
+<script>
+
+window.renderMermaid = async function(eltId) {
+  await loadMermaid();
+  mermaid.mermaidAPI.initialize({ startOnLoad: false });
+    // ^ Yes, we have to reload and reinitialize every time.
+    //   No, I don't know why.
+
+  await domContentLoaded;
+
+  const $in = document.getElementById(eltId);
+  const $out = document.createElement('div');
+  $out.style.textAlign = 'center';
+  $in.after($out);
+
+  mermaid.mermaidAPI.render($in.id, $in.innerText, r => { $out.innerHTML = r; });
+}
+
+const domContentLoaded = new Promise(resolve =>
+  document.addEventListener('DOMContentLoaded', () => resolve()));
+
+async function loadMermaid() {
+  const $script = document.createElement('script');
+  $script.src = 'https://cdn.jsdelivr.net/npm/mermaid/dist/mermaid.min.js';
+  await new Promise(resolve => {
+    $script.onload = () => resolve();
+    document.body.append($script);
+  });
+}
+
+</script>
+
+`;
